44ed8ab2a689bb5173753fe6cc5ab38331d759f7
Nenad Jeckovic <jeckovic.ra85.2018@uns.ac.rs>
2022-08-19 17:44:34 +0200

    feat: navbar

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentRecommendationComponent } from './component-recommendation/component-recommendation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'component-recommendation', component: ComponentRecommendationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentRecommendationComponent } from './component-recommendation/component-recommendation.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentRecommendationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
